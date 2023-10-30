9bb81d145f9fab81a2b3eaf91bbdf3b41109c6a8
Acane <listencpp@gmail.com>
2023-10-31 00:50:18 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# 中文数字转阿拉伯数字
+
+## 产生式
+
+``` 
+   Oku -> Man 亿 Man | Man
+   Man -> Sen 万 Sen | Sen
+   Sen -> NonZero 千 Hyaku | NonZero 千 零 Juu | Hyaku
+   Hyaku -> NonZero 百 Juu | NonZero 百 零 Num |
+   Juu -> NonZero 十 Num | 十 Num | Num
+   Num -> NonZero | 零
+   NonZero -> 一 | 二 | 三 | 四 | 五 | 六 | 七 | 八 | 九
+```
+
+## 使用方法
+
+```python 
+from chn_num_conv import *
+
+
+if __name__ == '__main__':
+    chars = [
+        "小米十三Pro十六加一百二十八G要三千九百九十九元，比红米K六零Pro贵",
+        "中国有十四亿一千七十七万八千七百二十四人，二万亿GDP",  # 1,411,778,724
+        "一百万五千九百九十五英镑",
+        "《一千零一夜》只卖三十五元",
+        "红米十二一百二十八GB多少钱？",
+        "他的电话是一三八一二三四五六七八",
+        "今天是二零二三年十月三十一日",
+        "刚刚清点了今天的收入，总共九万八千五百二十一点一零元"
+    ]
+    for ch in chars:
+        cc = ChineseNumberConvertor(ch)()
+        print(cc)
+```
+
+输出：
+``` 
+小米13Pro16加128G要3999元，比红米K60Pro贵
+中国有1410778724人，2000000000000GDP
+1005995英镑
+《1001夜》只卖35元
+红米12 128GB多少钱？
+他的电话是13812345678
+今天是2023年10月31日
+刚刚清点了今天的收入，总共98521.10元
+```
---
+++ chn_num_conv.py
@@ -0,0 +1,129 @@
+
+
+'''
+   Oku -> Man 亿 Man | Man
+   Man -> Sen 万 Sen | Sen
+   Sen -> NonZero 千 Hyaku | NonZero 千 零 Juu | Hyaku
+   Hyaku -> NonZero 百 Juu | NonZero 百 零 Num |
+   Juu -> NonZero 十 Num | 十 Num | Num
+   Num -> NonZero | 零
+   NonZero -> 一 | 二 | 三 | 四 | 五 | 六 | 七 | 八 | 九
+'''
+
+class ChineseNumberConvertor():
+    def __init__(self, s):
+        self.chn_num = "零一二三四五六七八九"
+        self.non_zero_num = "一二三四五六七八九"
+        self.chn_dict = {}
+        for i, c in enumerate(self.chn_num):
+            self.chn_dict[c] = i
+        self.i = -1
+        self.s = s
+        self.p = ""
+        self._out = None
+
+    def _next(self):
+        self.i += 1
+        if self.i >= self.s.__len__():
+            self.p = 'EOF'
+            return 'EOF'
+        self.p = self.s[self.i]
+        return self.p
+
+    def _retract(self):
+        self.i -= 1
+        self.p = self.s[self.i]
+        return self.p
+
+    def _N(self):
+        if self.p in self.chn_num:
+            n = self.chn_dict[self.p]
+            self._next()
+            return n
+        else:
+            return -1
+
+    def _J(self):
+        n = self._N()
+        if self.p == '十':
+            self._next()
+            m = self._N()
+            return max(1, n) * 10 + max(0, m)
+        return max(0, n)
+
+    def _H(self):
+        n = self._N()
+        if self.p == '百':
+            self._next()
+            if self.p == '零':
+                self._next()
+                m = self._N()
+            else:
+                m = self._J()
+            return max(1, n) * 100 + max(0, m)
+        if n >= 0:
+            self._retract()
+        return self._J()
+
+    def _S(self):
+        n = self._N()
+        if self.p == '千':
+            self._next()
+            if self.p == '零':
+                self._next()
+                m = self._J()
+            else:
+                m = self._H()
+            return max(1, n) * 1000 + m
+        if n >= 0:
+            self._retract()
+        return self._H()
+
+    def _M(self):
+        n = self._S()
+        if self.p == '万':
+            self._next()
+            if self.p == '零':
+                self._next()
+            m = self._S()
+            return n * 10000 + m
+        return n
+
+    def _O(self):
+        n = self._M()
+        if self.p == '亿':
+            self._next()
+            if self.p == '零':
+                self._next()
+            m = self._M()
+            return n * 1e8 + m
+        return n
+
+    def _start(self):
+        out = ""
+        self._next()
+        last_is_num = 0
+        while self.p != "EOF":
+            if self.p in self.chn_num or self.p == '十':
+                num = int(self._O())
+                # 单个的数字就不需要加空格了
+                if last_is_num and num > 10:
+                    out += " "
+                out += str(num)
+                last_is_num = 1
+            else:
+                if self.p == '点' and last_is_num:
+                    out += "."
+                else:
+                    out += self.p
+                last_is_num = 0
+                self._next()
+        self._out = out
+        return out
+
+    def __call__(self):
+        if self._out is not None:
+            return self._out
+        return self._start()
+
+
